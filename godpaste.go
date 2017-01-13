0eb2577d41e87bbbb3f660e6d6c7faa7776081c6
Erfan Besharat <erbesharat@gmail.com>
2017-01-13 15:56:47 +0330

    :sparkles: Added syntax option

--- godpaste.go
+++ godpaste.go
@@ -12,12 +12,14 @@ import (
 func checkerr(e error, message string) {
 	if e != nil {
 		fmt.Println(message)
+		os.Exit(10)
 	}
 }
 
 func main() {
 	var file string
 	var expire string
+	var syntax string
 	v := url.Values{}
 
 	app := cli.NewApp()
@@ -35,37 +37,45 @@ func main() {
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:        "file",
-			Value:       "empty",
+			Name:        "file, f",
+			Value:       "none",
 			Usage:       "Your text's file",
 			Destination: &file,
 		},
 		cli.StringFlag{
-			Name:        "expire",
-			Value:       "empty",
+			Name:        "expire, e",
+			Value:       "none",
 			Usage:       "Item's expiry date",
 			Destination: &expire,
+		},
+		cli.StringFlag{
+			Name:        "syntax, s",
+			Value:       "none",
+			Usage:       "Set syntax for your item",
+			Destination: &syntax,
 		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		if file != "empty" && expire == "empty" {
+		if file != "none" && expire == "none" {
 			text, err := ioutil.ReadFile(file)
 			checkerr(err, "Couldn't find your file")
 			v.Add("content", string(text))
+			v.Add("syntax", syntax)
 			resp, err := http.PostForm("http://dpaste.com/api/v2/", v)
 			checkerr(err, "Couldn't create your dpaste")
 			output, _ := resp.Location()
 			fmt.Printf("Check ==> %q", output)
-		} else if file != "empty" && expire != "empty" {
+		} else if file != "none" && expire != "none" {
 			text, err := ioutil.ReadFile(file)
 			checkerr(err, "Couldn't find your file")
 			v.Add("content", string(text))
 			v.Add("expiry_days", expire)
+			v.Add("syntax", syntax)
 			resp, err := http.PostForm("http://dpaste.com/api/v2/", v)
 			checkerr(err, "Couldn't create your dpaste")
 			link, _ := resp.Location()
-			fmt.Printf("link ==> %q, Expire: %q days", link, expire)
+			fmt.Printf("link ==> %q ---- Expires in %q days", link, expire)
 		} else {
 			fmt.Println("Please give a file. For more information check --help")
 		}
