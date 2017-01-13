776cf988dc04fba8a171e83c0d9e2587ca660fab
Erfan Besharat <erbesharat@gmail.com>
2017-01-13 15:40:48 +0330

    :sparkles: Added expiry days option

--- godpaste.go
+++ godpaste.go
@@ -16,6 +16,9 @@ func checkerr(e error, message string) {
 }
 
 func main() {
+	var file string
+	var expire string
+	v := url.Values{}
 
 	app := cli.NewApp()
 
@@ -30,18 +33,39 @@ func main() {
 	}
 	app.Version = "0.1"
 
-	app.Action = func(c *cli.Context) error {
-		if len(c.Args()) != 0 {
-			input := c.Args().First()
-			text, err := ioutil.ReadFile(input)
-			checkerr(err, "Couldn't find your file")
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "file",
+			Value:       "empty",
+			Usage:       "Your text's file",
+			Destination: &file,
+		},
+		cli.StringFlag{
+			Name:        "expire",
+			Value:       "empty",
+			Usage:       "Item's expiry date",
+			Destination: &expire,
+		},
+	}
 
-			v := url.Values{}
+	app.Action = func(c *cli.Context) error {
+		if file != "empty" && expire == "empty" {
+			text, err := ioutil.ReadFile(file)
+			checkerr(err, "Couldn't find your file")
 			v.Add("content", string(text))
 			resp, err := http.PostForm("http://dpaste.com/api/v2/", v)
 			checkerr(err, "Couldn't create your dpaste")
 			output, _ := resp.Location()
 			fmt.Printf("Check ==> %q", output)
+		} else if file != "empty" && expire != "empty" {
+			text, err := ioutil.ReadFile(file)
+			checkerr(err, "Couldn't find your file")
+			v.Add("content", string(text))
+			v.Add("expiry_days", expire)
+			resp, err := http.PostForm("http://dpaste.com/api/v2/", v)
+			checkerr(err, "Couldn't create your dpaste")
+			link, _ := resp.Location()
+			fmt.Printf("link ==> %q, Expire: %q days", link, expire)
 		} else {
 			fmt.Println("Please give a file. For more information check --help")
 		}
